4da1b37752b182c0a8f285043b221fa7d0ed161d
emlag <edl926@g.harvard.edu>
2018-07-28 12:05:11 +0800

    initial commit

---
+++ src/edu/cis/lab1/Main.java
@@ -0,0 +1,48 @@
+package edu.cis.lab1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+   /* TODO: Create a StringModifier class.
+         a. Place instance variables at the top.
+            You'll need a modifiedString variable and
+            an originalString variable.
+         b. All your methods should have one String argument.
+         c. Create, stringToUpperCase, a method that upper cases the original string and returns the new string.
+         d. Create, stringToLowerCase, a method that lower cases the original string and returns the new string.
+         e. Create a method, asToEs, that replaces all a's in a string for e's.
+         f. Create a method, combineStrings, that takes in two strings combines them.
+            So if the strings are "united" and "states", return "unitedstates".
+         g. Create a method, combineFirstThree, that takes in two strings and combines the first three
+            letters. So if the strings ate "united" and "states" return "unista".
+         Challenge: Create a method that changes the first letter of
+            ANY string to "YAS". So, "c.i.s." would turn into "YAS.i.s."
+
+      TODO: Create a StringSplitter class.
+         a. Create a method, splitThis, that takes in a string and splits
+            it into individual letters. So "unit" returns ["u","n","i","t"]. Return the resulting array.
+         b. Create a method, splitLongString, that takes in a sentences and splits it into
+            individual words. Return the string array.
+         Challenge: Create a method that splits any string in half and returns
+            both parts. So the string "Mexico" would return ["Mex", "ico"].
+      TODO: Create a StringCreator class.
+         a. Create a method, fromArrayToString, that takes in an Array of chars and returns a whole string.
+         b. Create a method, fromTwoArraysToString,  that takes in two Arrays of chars and returns one whole string.
+
+      TODO: Create a StringInfo class.
+         a. Create a method, lengthOfString, that returns the length of a given string.
+         b. Create a method, stringIsEqual, that takes in two strings and returns true if
+            the strings are the same, otherwise return false.
+         Challenge :  Create a method that returns the index of the character "c" in any string.
+
+      TODO: Create a StringReader class.
+         a. Create a method, readFromUser, that reads the user's input.-
+
+    */
+}
+
